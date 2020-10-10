a1a77ecfad4db98f7a87b9ea2a17dcd8f2c36f37
yidayoung <418621207@qq.com>
2020-10-10 17:02:46 +0800

    fix warring of OTP23

--- src/pbkdf2.erl
+++ src/pbkdf2.erl
@@ -134,12 +134,21 @@ pbkdf2(MacFunc, Password, Salt, Iterations, BlockIndex, Iteration, Prev, Acc) ->
 %-type mac_func_info() :: mac_func() | {hmac, digest_func_info()}
 %	| md4 | md5 | ripemd160 | sha | sha224 | sha256 | sha384 | sha512.
 
+-if(?OTP_RELEASE < 23).
+-define(HMAC(DigestFunc, Key, Data),
+	HMAC = crypto:hmac_init(DigestFunc, Key),
+	HMAC1 = crypto:hmac_update(HMAC, Data),
+	crypto:hmac_final(HMAC1)).
+-else.
+-define(HMAC(DigestFunc, Key, Data),
+	HMAC = crypto:mac_init(hmac, DigestFunc, Key),
+	HMAC1 = crypto:mac_update(HMAC, Data),
+	crypto:mac_final(HMAC1)).
+-endif.
+
 resolve_mac_func({hmac, DigestFunc}) ->
 	fun(Key, Data) ->
-		%crypto:hmac(DigestFunc, Key, Data)
-		HMAC = crypto:hmac_init(DigestFunc, Key),
-		HMAC1 = crypto:hmac_update(HMAC, Data),
-		crypto:hmac_final(HMAC1)
+		?HMAC(DigestFunc, Key, Data)
 	end;
 
 resolve_mac_func(MacFunc) when is_function(MacFunc) ->
